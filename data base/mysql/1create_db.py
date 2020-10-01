d5ba8d140324ff28b9bd2a940823c09b623096b4
prateek agrawal <prateekagrawal252000@gmail.com>
2020-10-01 12:29:50 +0530

    Update 1create_db.py

--- data base/mysql/1create_db.py
+++ data base/mysql/1create_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root",passwd="Mysql12345!@#$%")
+mydb = mysql.connector.connect(host="localhost", user="root",passwd="your_password")
 
 mycursor = mydb.cursor()
 
