d7dd33bc1e3274ee976380b5722e90fc2fc0db91
prateek agrawal <prateekagrawal252000@gmail.com>
2020-10-15 18:25:52 +0530

    Update 2show_db.py

--- data base/mysql/2show_db.py
+++ data base/mysql/2show_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root",passwd="Mysql12345!@#$%")
+mydb = mysql.connector.connect(host="localhost", user="root",passwd="YOUR PASSWORD OF DATABASES")
 
 mycursor = mydb.cursor()
 mycursor.execute("SHOW DATABASES")
